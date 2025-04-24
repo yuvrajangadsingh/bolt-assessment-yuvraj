de84a6dcfb1f6dc96028c5cfc14dbb70b5ff7ea0
Yuvraj Angad Singh <yuvrajangad.s@gmail.com>
2025-04-25 00:23:58 +0530

    Add endpoint to fetch D&D class details by class name

--- src/main/java/com/bolt/assessment/controller/DndController.java
+++ src/main/java/com/bolt/assessment/controller/DndController.java
@@ -1,9 +1,11 @@
 package com.bolt.assessment.controller;
 
 import com.bolt.assessment.model.ApiSummary;
+import com.bolt.assessment.model.ClassDetail; 
 import com.bolt.assessment.service.DndApiService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class DndController {
     @GetMapping("/summary")
     public ResponseEntity<ApiSummary> getSummary() {
         return ResponseEntity.ok(dndApiService.getApiSummary());
+    }
+
+    @GetMapping("/classes/{className}")
+    public ResponseEntity<ClassDetail> getClassDetails(@PathVariable String className) {
+        return ResponseEntity.ok(dndApiService.getClassDetails(className));
     }
 } 
